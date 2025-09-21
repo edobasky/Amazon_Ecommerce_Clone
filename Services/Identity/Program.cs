93b79c857070e759c53b1be2ce570ad1ba5a621c
edobasky <edore7@gmail.com>
2025-09-21 08:30:47 +0100

    added jwt setup

--- Services/Identity/Program.cs
+++ Services/Identity/Program.cs
@@ -1,7 +1,12 @@
+using Identity.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// add ef
+builder.Services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
